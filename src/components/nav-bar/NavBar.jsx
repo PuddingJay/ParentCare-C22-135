7cbba2d5a0dd396b3b478a9d193e201d2e21dc44
PuddingJay <muhammadwindu27@gmail.com>
2022-12-02 00:02:55 +0700

    add Navbar for Layout, add react-icons

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import './navBar.scss';
+import { Link } from 'react-router-dom';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { FiHome } from 'react-icons/fi';
+import { CgSearch } from 'react-icons/cg';
+import { BsPersonCircle } from 'react-icons/bs';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="navBar">
+      <div className="left">
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <span>ParentCare</span>
+        </Link>
+        <FiHome />
+        <FaMoon />
+        <div className="search">
+          <CgSearch />
+          <input type="text" placeholder="Search" />
+        </div>
+
+      </div>
+      <div className="right">
+        <BsPersonCircle />
+        <div className="user">
+          <span>Windu</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
