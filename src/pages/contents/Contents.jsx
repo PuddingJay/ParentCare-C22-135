fd3d4c69c9b2b5ebfd4182a42eb6caac6e3a02d4
PuddingJay <muhammadwindu27@gmail.com>
2022-12-08 16:19:49 +0700

    update content appearance

--- src/pages/contents/Contents.jsx
+++ src/pages/contents/Contents.jsx
@@ -10,13 +10,13 @@ const Contents = ({ name }) => {
   function onAddContentHandler({ title, body }) {
     setContent(() => {
       return [
-        ...content,
         {
           id: +new Date(),
           createdAt: +new Date(),
           title,
           body,
         },
+        ...content,
       ];
     });
     console.log(content);
@@ -28,23 +28,23 @@ const Contents = ({ name }) => {
       {content.map((content) => (
         <div className="content" key={content.id}>
           <div className="user">
-            <div className="item__image">
+            <div className="user__info">
               <img
-                className="user__image"
-                src={`https://ui-avatars.com/api/?name=${
-                  content.name === undefined ? name : content.name
-                }&background=random`}
+                src={`https://ui-avatars.com/api/?name=${content.name === undefined ? name : content.name
+                  }&background=random`}
                 alt="name avatar"
               />
-              <h4 className="name">
-                {content.name === undefined ? name : content.name}
-              </h4>
+              <div className="made">
+                <span className="name">
+                  {content.name === undefined ? name : content.name}
+                </span>
+                <span className="date">Recently added at {showFormattedDate(content.createdAt)}</span>
+              </div>
             </div>
-            <h2>{content.title}</h2>
-            <p>{showFormattedDate(content.createdAt)}</p>
           </div>
           <div className="body__content">
-            <h4>{content.body}</h4>
+            <span>{content.title}</span>
+            <span>{content.body}</span>
           </div>
         </div>
       ))}
